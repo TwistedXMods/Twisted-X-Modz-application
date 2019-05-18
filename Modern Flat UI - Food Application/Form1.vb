fe303bbd2acb905c7f27da3ed6b25a50fc4317f9
TwistedExecutor <48439244+TwistedExecutor@users.noreply.github.com>
2019-05-18 17:35:51 -0400

    Twisted X Modz Application V5
    Copyright (C) 2019 Twisted X Modz
    
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
    as published by the Free Software Foundation; either version 2
    of the License, or (at your option) any later version.
    
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
    
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
    Also add information on how to contact you by electronic and paper mail.
    
    If the program is interactive, make it output a short notice like this when it starts in an interactive mode:
    
    Gnomovision version 69, Copyright (C) year name of author
    Gnomovision comes with ABSOLUTELY NO WARRANTY; for details
    type `show w'.  This is free software, and you are welcome
    to redistribute it under certain conditions; type `show c'
    for details.
    The hypothetical commands `show w' and `show c' should show the appropriate parts of the General Public License. Of course, the commands you use may be called something other than `show w' and `show c'; they could even be mouse-clicks or menu items--whatever suits your program.
    
    signature of Ty Coon, 1 April 1989
    Ty Coon, President of Vice

---
+++ Modern Flat UI - Food Application/Form1.vb
@@ -0,0 +1,189 @@
+﻿Public Class frmMain
+    Private Sub btnFoods_Click(sender As Object, e As EventArgs) Handles btnFoods.Click
+        Process.Start("http://terrormodz.tk")
+    End Sub
+
+    Private Sub btnDrinks_Click(sender As Object, e As EventArgs) Handles btnDrinks.Click
+
+    End Sub
+
+    Private Sub btnDeserts_Click(sender As Object, e As EventArgs) Handles btnDeserts.Click
+        Form2.Show()
+    End Sub
+
+    Private Sub btnMyCart_Click(sender As Object, e As EventArgs) Handles btnMyCart.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\Twisted AI.exe")
+    End Sub
+
+    Private Sub btnAboutUs_Click(sender As Object, e As EventArgs) Handles btnAboutUs.Click
+        Form1.Show()
+    End Sub
+
+    Private Sub picClose_Click(sender As Object, e As EventArgs) Handles picClose.Click
+        Me.Close()
+    End Sub
+
+    Private Sub picMinimize_Click(sender As Object, e As EventArgs) Handles picMinimize.Click
+        Me.WindowState = WindowState.Minimized
+    End Sub
+
+    Private Sub picMaximize_Click(sender As Object, e As EventArgs) Handles picMaximize.Click
+        If Me.WindowState = WindowState.Normal Then
+            Me.WindowState = WindowState.Maximized
+        ElseIf Me.WindowState = WindowState.Maximized Then
+            Me.WindowState = WindowState.Normal
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Process.Start("https://discord.io/TwistedXModz")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Process.Start("https://twistedxmodz.com")
+        Process.Start("https://www.youtube.com/c/TwistedExecutor")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Process.Start("https://twistedxmodz.com/index.php?adcreditshop-categories/shop.1/view")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\RGHC.exe")
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\Smokey Bo1.exe")
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\Bo2 Shit.exe")
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\R34sMods.exe")
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\ZambiesV3.exe")
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\GtaV 100% Save Injector.exe")
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\Tsunami.exe")
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\Project Rebirth.exe")
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\Blue Sky Tool.exe")
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\VexSpoofer.exe")
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\KV_Checker.exe")
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\RecoveryTool.exe")
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\MSP-Spoofer.exe")
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        Dim Ip As String
+        Ip = TextBox1.Text
+        If My.Computer.Network.Ping(Ip) Then
+            Label3.ForeColor = Color.Green
+            MsgBox("Online")
+        Else
+            Label3.ForeColor = Color.Red
+            MsgBox("Offline")
+        End If
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs)
+        Process.Start("C:\Program Files (x86)\Twisted Modz\XBOX360 SDK.exe")
+    End Sub
+
+    Private Sub Button21_Click(sender As Object, e As EventArgs)
+        Process.Start("C:\Program Files (x86)\Twisted Modz\rufus-3.4.exe")
+    End Sub
+
+    Private Sub Button20_Click(sender As Object, e As EventArgs)
+        Process.Start("C:\Program Files (x86)\Twisted Modz\VMware 14.exe")
+    End Sub
+
+    Private Sub Button19_Click(sender As Object, e As EventArgs)
+        Process.Start("C:\Program Files (x86)\Twisted Modz\VirtualBox.exe")
+    End Sub
+
+    Private Sub Button18_Click_1(sender As Object, e As EventArgs) Handles Button18.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\XBOX360 SDK.exe")
+    End Sub
+
+    Private Sub Button19_Click_1(sender As Object, e As EventArgs) Handles Button19.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\rufus-3.4.exe")
+    End Sub
+
+    Private Sub Button20_Click_1(sender As Object, e As EventArgs) Handles Button20.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\VMware 14.exe")
+    End Sub
+
+    Private Sub Button21_Click_1(sender As Object, e As EventArgs) Handles Button21.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\VirtualBox.exe")
+    End Sub
+
+    Private Sub Button22_Click(sender As Object, e As EventArgs) Handles Button22.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\Notepad++Installer.exe")
+    End Sub
+
+    Private Sub Button23_Click(sender As Object, e As EventArgs) Handles Button23.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\DaVinci Resolve.exe")
+    End Sub
+
+    Private Sub Button24_Click(sender As Object, e As EventArgs) Handles Button24.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\OBS Studio.exe")
+    End Sub
+
+    Private Sub Button32_Click(sender As Object, e As EventArgs) Handles Button32.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\putty 64bit.exe")
+    End Sub
+
+    Private Sub Button31_Click(sender As Object, e As EventArgs) Handles Button31.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\Visual Studio 2017.exe")
+    End Sub
+
+    Private Sub Button30_Click(sender As Object, e As EventArgs) Handles Button30.Click
+        Process.Start("C:\Program Files (x86)\Twisted Modz\winrar-x64-561.exe")
+    End Sub
+
+    Private Sub Button25_Click(sender As Object, e As EventArgs) Handles Button25.Click
+        Dim FILE_NAME As String = "C:\Program Files (x86)\Twisted Modz\VMware 14 pro keys.txt"
+
+        If System.IO.File.Exists(FILE_NAME) = True Then
+            Process.Start(FILE_NAME)
+        Else
+            MsgBox("File Does Not Exist")
+        End If
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub pnlTopSide_Paint(sender As Object, e As PaintEventArgs) Handles pnlTopSide.Paint
+
+    End Sub
+End Class
---
+++ Modern Flat UI - Food Application/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Twisted X Modz")>
+<Assembly: AssemblyDescription("Twisted X Modz")>
+<Assembly: AssemblyCompany("Twisted X Modz")>
+<Assembly: AssemblyProduct("Twisted X Modz")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("da27911a-872d-4638-a1aa-0905b698038c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("4.8.7.15")>
+<Assembly: AssemblyFileVersion("5.10.8.20")>
